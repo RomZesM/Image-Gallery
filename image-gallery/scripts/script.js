e86184fe3ba8e8d26f3a83df3673df266159db5c
RomZesM <dwarf.md@gmail.com>
2023-09-28 11:33:46 +0200

    feat: add start imagelist from local json file

--- image-gallery/scripts/script.js
+++ image-gallery/scripts/script.js
@@ -15,31 +15,31 @@ let accesKey = "Abed2b9A8CYciNLGC3Ilzfwkw9Lh4-aINn6yKl7ZOxc"
 
 let objectFromApi = null;
 
-
+printLocalJSON();
 
 searchForm.addEventListener("submit", (event)=>{
 	let generatedUrl = '';
-	console.log(searchForm.elements[0].value.replaceAll(' ', "%20"));
-	let searchRequest = searchForm.elements[0].value.replaceAll(' ', "%20")
+	if(searchForm.elements[0].value == ""){ //if there is empty query - show us a pictures from local js
+		printLocalJSON();
+	}
+	//console.log(searchForm.elements[0].value.replaceAll(' ', "%20"));
+	let searchRequest = searchForm.elements[0].value.replaceAll(' ', "%20")//replace spaces in user query
 	generatedUrl = "https://api.unsplash.com/search/photos?query=" + searchRequest + "&per_page=30&orientation=landscape&client_id=Abed2b9A8CYciNLGC3Ilzfwkw9Lh4-aINn6yKl7ZOxc"
 	
 	getData(generatedUrl);
 	//showImages2()
-	console.log(generatedUrl);
+	//console.log(generatedUrl);
 	event.preventDefault();//prevent submition of the form	
 });
 
-
-
-
-
+//get JSON from API
 async function getData(url) {
 	console.log("start request");
 	const res = await fetch(url);
 	const data = await res.json();
-	console.log(data);
+	//console.log(data);
 	
-	//localStorage.setItem('testObject', JSON.stringify(data));
+	//localStorage.setItem('testObject', JSON.stringify(data)); //save json in Local storage
 	
 	showImages(data);
   }
@@ -48,6 +48,9 @@ async function getData(url) {
 	//var retrievedObject = JSON.parse(localStorage.getItem('testObject'));
 	//console.log('retrievedObject: ', retrievedObject);
 	
+	
+	galleryContainer.innerHTML = ""; //clear old file
+
 	for (let i = 0; i < data.results.length; i++) {
 			const img = document.createElement('img');
 			img.classList.add('gallery-img')
@@ -56,11 +59,12 @@ async function getData(url) {
 			galleryContainer.append(img);
 	}
   }
-  //dele
-  function showImages2(){
+  //!delete just test stub function
+    function showImages2(){
 	var retrievedObject = JSON.parse(localStorage.getItem('testObject'));
-	console.log('retrievedObject: ', retrievedObject);
-	
+
+	galleryContainer.innerHTML = "";
+
 	for (let i = 0; i < retrievedObject.results.length; i++) {
 			const img = document.createElement('img');
 			img.classList.add('gallery-img')
@@ -70,5 +74,14 @@ async function getData(url) {
 	}
   }
  
+//start first and read from local json  
+  async function printLocalJSON() {
+	const response = await fetch("assets/json/start.json");
+	const json = await response.json();
+
+	showImages(json)
+}
+
+
 
  
