d5c4fae6b49eee1a6998da8b42351d90b30c3014
RomZesM <dwarf.md@gmail.com>
2023-10-01 11:11:50 +0200

    feat: add error page for empty request

--- image-gallery/scripts/script.js
+++ image-gallery/scripts/script.js
@@ -20,11 +20,11 @@ searchForm.addEventListener("submit", (event)=>{
 	if(searchForm.elements[0].value == ""){ //if there is empty query - show us a pictures from local js
 		printLocalJSON();
 	}
-	//console.log(searchForm.elements[0].value.replaceAll(' ', "%20"));
 	let searchRequest = searchForm.elements[0].value.replaceAll(' ', "%20")//replace spaces in user query
 	generatedUrl = "https://api.unsplash.com/search/photos?query=" + searchRequest + "&per_page=30&orientation=landscape&client_id=Abed2b9A8CYciNLGC3Ilzfwkw9Lh4-aINn6yKl7ZOxc"
 	
 	getData(generatedUrl);
+	//showError();
 	event.preventDefault();//prevent submition of the form	
 });
 
@@ -49,7 +49,7 @@ async function getData(url) {
 	const res = await fetch(url);
 	const data = await res.json();
 		
-	//localStorage.setItem('testObject', JSON.stringify(data)); //save json in Local storage
+	localStorage.setItem('emptyRequest', JSON.stringify(data)); //save json in Local storage
 	
 	showImages(data);
   }
@@ -57,43 +57,35 @@ async function getData(url) {
   function showImages(data){
 	//var retrievedObject = JSON.parse(localStorage.getItem('testObject'));
 	//console.log('retrievedObject: ', retrievedObject);
-		
-	galleryContainer.innerHTML = ""; //clear old file
+	if(data.results.length === 0){
+		galleryContainer.innerHTML = ""; //clear old screen
 
-	for (let i = 0; i < data.results.length; i++) {
-		const div_img = document.createElement('div');
-			div_img.classList.add('gallery-img');
-			div_img.style.backgroundImage = `url(${data.results[i].urls.regular})`;
-			
-			div_img.addEventListener("click", (e)=>{
-				modal_overlay.classList.add("modal-overlay-visible");
-				big_picture.style.backgroundImage = `url(${data.results[i].urls.regular})`;
-			})			
-			
-			galleryContainer.append(div_img);
-		}
+		const div_empty = document.createElement('div');
+		div_empty.classList.add('error-message');
+		div_empty.style.backgroundImage = `url(/assets/img/error.png)`
+		div_empty.innerHTML = "Nothing was found in UNSPLASH API, try another request"
+
+		galleryContainer.append(div_empty);
+	}	
+	else{
+		galleryContainer.innerHTML = ""; //clear old file
+
+		for (let i = 0; i < data.results.length; i++) {
+			const div_img = document.createElement('div');
+				div_img.classList.add('gallery-img');
+				div_img.style.backgroundImage = `url(${data.results[i].urls.regular})`;
+				
+				div_img.addEventListener("click", (e)=>{
+					modal_overlay.classList.add("modal-overlay-visible");
+					big_picture.style.backgroundImage = `url(${data.results[i].urls.regular})`;
+				})			
+				
+				galleryContainer.append(div_img);
+			}
+	}
+
 	}
   
-//   //!delete just test stub function
-//     function showImages2(){
-// 	var retrievedObject = JSON.parse(localStorage.getItem('testObject'));
-
-// 	galleryContainer.innerHTML = "";
-
-// 	for (let i = 0; i < retrievedObject.results.length; i++) {
-// 			const div_img = document.createElement('div');
-// 			div_img.classList.add('gallery-img');
-// 			div_img.style.backgroundImage = `url(${retrievedObject.results[i].urls.regular})`;
-			
-// 			//show image by click
-// 			div_img.addEventListener("click", (e)=>{
-// 				modal_overlay.classList.add("modal-overlay-visible");
-// 				big_picture.style.backgroundImage =  `url(${retrievedObject.results[i].urls.regular})`;
-// 			})
-			
-// 			galleryContainer.append(div_img);
-// 	}
-//   }
  
 //start first and read from local json  
   async function printLocalJSON() {
@@ -105,5 +97,17 @@ async function getData(url) {
 }
 
 
+function showError(){
+	
+	console.log("Bad request");
+	
+	galleryContainer.innerHTML = ""; //clear old file
 
+	const div_empty = document.createElement('div');
+	 div_empty.classList.add('error-message');
+	 div_empty.style.backgroundImage = `url(/assets/img/error.png)`
+	 div_empty.innerHTML = "Nothing was found in UNSPLASH API, try another request"
+
+	galleryContainer.append(div_empty);
+}
  
